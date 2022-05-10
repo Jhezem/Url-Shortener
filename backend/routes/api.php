531b8e3266fdf76630a4bf42977b9ac92957de67
Jhezem <mmartinhez08@gmail.com>
2022-05-10 12:26:28 -0600

    short route added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,3 +20,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::get('most-viewed', [UrlController::class, 'index']);
+Route::get('short', [UrlController::class, 'store']);
