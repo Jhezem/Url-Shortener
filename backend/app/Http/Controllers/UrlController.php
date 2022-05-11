e03fb5660631332fa8f836099c792be9f14a653e
Jhezem <mmartinhez08@gmail.com>
2022-05-11 14:02:28 -0600

    Added store, show, most viewe and index methods

--- backend/app/Http/Controllers/UrlController.php
+++ backend/app/Http/Controllers/UrlController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Url;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 
@@ -16,7 +18,25 @@ class UrlController extends Controller
      */
     public function index()
     {
-        return Url::all();
+       return Url::all();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function most_viewed()
+    {
+        $most_viewed = Url::orderBy('views', 'desc')->take(20)->get();
+
+        if($most_viewed){
+            return $most_viewed;
+        }
+
+        return response()->json([
+            'res' => 204,
+        ]);  
     }
 
     /**
@@ -27,26 +47,56 @@ class UrlController extends Controller
      */
     public function store(Request $request)
     {
-        if($request->original_url){
-            $new_url = new Url();
-            $new_url->original_url = $request->original_url;
-            $new_url->views = 0;
-            $new_url->shorten_url = Str::random(6);
-            $new_url->save();
 
-            return $new_url;
+        if($request->original_url){
+
+            if(!filter_var($request->original_url, FILTER_VALIDATE_URL)){
+                return response()->json([
+                    'res' => false,
+                    'msg' => 'Bad url provided, include http or https'
+                ]);                
+            }
+
+            $newUrl = Url::create([
+                'original_url' => $request->original_url,
+                'views' => 0,
+                'shorten_url' => Str::random(6),
+            ]);
+
+            return response()->json([
+                'res' => true,
+                'msg' => 'url shortened succesfully',
+                'data' => $newUrl
+            ]);
         }
+
+        return response()->json([
+            'res' => false,
+            'msg' => 'Not url shortened'
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  string  $key
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($key)
     {
-        //
+        $short_url = Url::where('shorten_url', $key)->first();
+
+        if($short_url){
+            $short_url->update([
+                'views' => $short_url->views + 1
+            ]);
+            return $short_url;
+        }
+
+        return response()->json([
+            'res' => 204,
+            'msg' => 'no link found'
+        ]);  
     }
 
     /**
