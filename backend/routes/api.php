01902c00d2ce62c51fa9eb5a3a09e767665af797
Jhezem <mmartinhez08@gmail.com>
2022-05-11 14:03:49 -0600

    Added all routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -19,5 +19,15 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('most-viewed', [UrlController::class, 'index']);
-Route::get('short', [UrlController::class, 'store']);
+//get all urls
+Route::get('all', [UrlController::class, 'index']);
+
+//get 20 most viewed urls
+Route::get('most-viewed', [UrlController::class, 'most_viewed']);
+
+// create shorten url most be logged in
+Route::middleware(['auth:sanctum'])->post('short', [UrlController::class, 'store']);
+
+
+//get shorten url
+Route::get('shorten-url/{key}', [UrlController::class, 'show']);
